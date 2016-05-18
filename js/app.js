39bf2f1e172de7e87f959b335ac7a7225e1f59c0
Sophie Price <sophimayp@gmail.com>
2016-05-19 11:52:06 +1200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -46,7 +46,7 @@ if (navigator.geolocation) {
              }else{
                  $("#3to4").show();
              }
-             var weatherTemp = json.threeHour.temperature;
+             var weatherTemp = json.threeHour.temp;
              var windLayers = json.threeHour.windProofLayers;
              document.getElementById("barTempDisp").innerHTML = weatherTemp + '°';
              document.getElementById("weatherDisp").innerHTML = "It's currently " + weatherTemp +" degrees in " + cityUpper + ".";
