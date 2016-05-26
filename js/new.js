a2d5b5093845fe2b8fc9f0e4dede2765afda985e
Sophie Price <sophimayp@gmail.com>
2016-05-26 14:50:30 +1200

    Update new.js

--- js/new.js
+++ js/new.js
@@ -35,6 +35,9 @@ function getGeolocation(){
 }
 
 function getFirebaseData(){
+	if(globalData.currentCity.equals("locate")){
+		getGeolocation();
+	}
     var fireURL = globalData.currentCity;
     fireURL = fireURL.replace(/\s+/g, '-').toLowerCase();
     var data = new Firebase("https://intense-fire-1222.firebaseio.com/" + fireURL +"/");
@@ -45,6 +48,9 @@ function getFirebaseData(){
 }
 
 function getWeatherData(){
+	if(globalData.currentCity.equals("locate")){
+		getGeolocation();
+	}
     var metServ = "http://uni.ey.nz/metservice.php?localObs_";
     var cityURL = globalData.currentCity;
     cityURL = cityURL.replace(/\s+/g, '-').toLowerCase(); //making lowercase and any spaces change to - so that it will not break URL and cities like New Plymouth become new-plymouth which works with the URL. 
@@ -180,13 +186,16 @@ $(document).ready(function(){
     getGeolocation();
     
     $("#inputCity").change(function () {
-    	if($(this).val().equals("locate")){
+    	/*if($(this).val().equals("locate")){
     		getGeolocation();
     	}else{
 	        globalData.currentCity = $(this).val();
 	        getWeatherData();
 	        getFirebaseData();
-    	}
+    	}*/
+    	globalData.currentCity = $(this).val();
+        getWeatherData();
+        getFirebaseData();
     });
     
     //initial hide the three parts of the app
